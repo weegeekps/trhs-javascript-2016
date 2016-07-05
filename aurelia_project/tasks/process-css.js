f645a18274f3bf360e3ebfa7927f6f8ea5ab3322
Adam Morris <adam@kernelpanicstudios.com>
2016-07-05 21:36:47 +0000

    Checking in what I have so far for getting foundation-sites installed. There are some jQuery errors I haven't solved yet.

--- aurelia_project/tasks/process-css.js
+++ aurelia_project/tasks/process-css.js
@@ -1,12 +1,18 @@
 import gulp from 'gulp';
 import sourcemaps from 'gulp-sourcemaps';
 import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
 
 export default function processCSS() {
   return gulp.src(project.cssProcessor.source)
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      includePaths: ['./node_modules/foundation-sites/scss/']
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3']
+    }))
     .pipe(build.bundle());
 };
